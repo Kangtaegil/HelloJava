555decd68b9a452381014a5d28af62377e764a91
강태길 <dmsrkql_8@naver.com>
2016-09-09 11:13:13 +0900

    마스터 브랜치

--- Project1/src/HelloJava.java
+++ Project1/src/HelloJava.java
@@ -5,6 +5,7 @@ public class HelloJava {
 		// TODO Auto-generated method stub
 		System.out.println("헬로 자바!");
 		System.out.println("2013041036 강태길");
+		System.out.println("여기는 마스터 브랜치");
 	}
 
 }
