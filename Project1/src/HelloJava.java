e35baad92b3e56145e203f217aafe53c09561352
태길 <@113.198.176.145>
2016-09-09 11:04:31 +0900

    add line

--- Project1/src/HelloJava.java
+++ Project1/src/HelloJava.java
@@ -4,6 +4,7 @@ public class HelloJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("헬로 자바!");
+		System.out.println("2013041036 강태길");
 	}
 
 }
