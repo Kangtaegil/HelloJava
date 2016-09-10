66e30a68dc7f510ff93db5f12b68fc251068efc1
강태길 <dmsrkql_8@naver.com>
2016-09-11 04:25:14 +0900

    GIthub and java

---
+++ Project1/src/GitJava.java
@@ -0,0 +1,10 @@
+
+public class GitJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("이클립스와 깃허브 연동을 이용하였습니다.");
+		System.out.println("2013041036 강태길");
+	}
+
+}
